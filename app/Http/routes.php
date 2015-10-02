99526a5a14b3143b6c5f5f1248d38862ee0ea3be
DusanMinic <dushanminic@gmail.com>
2015-10-02 16:57:33 +0200

    Basic Model/Controller/View Workflow

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("about", "PagesController@about");
+Route::get("contact", "PagesController@contact");
+
+Route::get("articles", "ArticlesController@index");
+Route::get("articles/{id}", "ArticlesController@show");
