585532ce967bcb7c24c89d1a706bef875f6ec8e7
DusanMinic <dushanminic@gmail.com>
2015-10-23 18:30:21 +0200

    Enhancing Select Elements(Select2)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<title>My First App</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
+	
 </head>
 <body>
 	<div class="container">
@@ -21,7 +23,8 @@
 		//	$("div.alert").not("alert-important").delay(3000).slideUp(3000);
 	</script>
 	
-	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
 	@yield("footer")
 </body>
 </html>
